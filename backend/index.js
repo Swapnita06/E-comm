effbca0df3a9fa77b68a947bb72a7fdef6b3bdd2
Swapnita Singh <swapimamuni@gmail.com>
2024-05-23 18:38:17 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const cors = require("cors");
-require('./db/config');
-const User = require("./db/User")
-const app = express();
+const express = require('express'); // Import the Express library
+const cors = require("cors"); // Import the CORS middleware
+require('./db/config'); // Connect to the database
+const User = require("./db/User") // Import the User model
+const app = express(); // Create an Express application
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Use the JSON parser middleware
+app.use(cors()); // Use the CORS middleware
 app.post("/register",async (req,resp)=>{
-    const user = new User(req.body);
-    let result= await user.save();
-    result = result.toObject();
-    delete result.password;
-       resp.send(result);
+    const user = new User(req.body); // Create a new User with the order details
+    let result= await user.save(); // Save the user in the database (kitchen)
+    result = result.toObject();// Convert the result to a standard format that is plain javascript object
+    delete result.password; // Remove the password before sending back to the customer
+       resp.send(result);// Send the response back to the customer
 })
 
 app.post("/login",async(req,resp)=>{
-    console.log(req.body)
-    if(req.body.password && req.body.email){
+    console.log(req.body) 
+    if(req.body.password && req.body.email){ // Check if both email and password are provided
 
-        let user = await  User.findOne(req.body).select("-password");
+        let user = await  User.findOne(req.body).select("-password"); // Find the user in the database
     if(user){
-        resp.send(user)
+        resp.send(user) // Send the user details back to the client
     }else{
-        resp.send("user not found")
+        resp.send("user not found") // Inform the customer that the user was not found
     }
     }else{
-        resp.send("please enter email and password")
+        resp.send("please enter email and password") // Ask the client to provide both email and password
     }
     
     
